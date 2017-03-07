a142ec38e9150cfabd178698ba0f049743d6e6d0
Nate Morris <nate.morris@marconet.com>
2017-03-07 13:23:14 -0600

    tweak Kinivio headphones script

--- profile/bin/fix-headphones.sh
+++ profile/bin/fix-headphones.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pactl set-card-profile bluez_card.00_1A_7D_B2_5E_45 headset_head_unit
+pacmd set-card-profile bluez_card.00_1A_7D_B2_5E_45 headset_head_unit
+sleep 1s
 echo -e "disconnect 00:1A:7D:B2:5E:45 \n exit" | bluetoothctl
+sleep 5s
 echo -e "connect 00:1A:7D:B2:5E:45 \n exit" | bluetoothctl
-pactl set-card-profile bluez_card.00_1A_7D_B2_5E_45 a2dp_sink
+sleep 5s
+pacmd set-card-profile bluez_card.00_1A_7D_B2_5E_45 a2dp_sink
+sleep 1s
+pacmd set-default-sink bluez_sink.00_1A_7D_B2_5E_45

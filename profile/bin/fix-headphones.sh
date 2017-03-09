3a46b6779b0685ea83112b78e5564d745b6f740c
Nate Morris <nate.morris@marconet.com>
2017-03-09 13:23:48 -0600

    script tweaks

--- profile/bin/fix-headphones.sh
+++ profile/bin/fix-headphones.sh
@@ -8,3 +8,4 @@ sleep 5s
 pacmd set-card-profile bluez_card.00_1A_7D_B2_5E_45 a2dp_sink
 sleep 1s
 pacmd set-default-sink bluez_sink.00_1A_7D_B2_5E_45
+pacmd set-default-source bluez_card.00_1A_7D_B2_5E_45

9d4ae4b441ba6b70b1db54c1b6890566ae00ae44
Nate Morris <nate.morris@marconet.com>
2017-08-29 10:21:20 -0500

    Fix rsync command

--- profile/install.sh
+++ profile/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Copy files to home
-rsync {--exclude\ \"{.git,.gitignore,*.swp,install.sh}\"} -avh --no-perms . ~
+rsync --exclude ".git/" \
+      --exclude ".gitignore" \
+      --exclude "*.swp" \
+      --exclude "install.sh" \
+      -avh --no-perms . ~
 
 # Remove install.sh, if it was moved there previously
-rm ~/install.sh
+if [ -f ~/install.sh ]; then
+    rm ~/install.sh
+fi
 
 # Refresh settings, in case new ones were loaded
 source ~/.bash_profile;

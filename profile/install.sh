2fd9c817e9d02333c297c1700daa18269667c986
Nate Morris <nate.morris@marconet.com>
2017-09-01 09:58:37 -0500

    Remove .git if it exists

--- profile/install.sh
+++ profile/install.sh
@@ -12,5 +12,10 @@ if [ -f ~/install.sh ]; then
     rm ~/install.sh
 fi
 
+# Remove .git folder, if it was moved there previously
+if [ -d ~/.git ]; then
+    rm -rf ~/.git
+fi
+
 # Refresh settings, in case new ones were loaded
 source ~/.bash_profile;

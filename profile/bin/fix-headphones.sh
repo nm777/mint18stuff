5e857f0a0186773f1d9f94ad25ff3c2bec580bec
Nate Morris <nate.morris@marconet.com>
2017-09-21 14:29:45 -0500

    Disable failing pacmd line

--- profile/bin/fix-headphones.sh
+++ profile/bin/fix-headphones.sh
@@ -6,6 +6,6 @@ sleep 5s
 echo -e "connect 00:1A:7D:B2:5E:45 \n exit" | bluetoothctl
 sleep 5s
 pacmd set-card-profile bluez_card.00_1A_7D_B2_5E_45 a2dp_sink
-sleep 1s
+sleep 3s
 pacmd set-default-sink bluez_sink.00_1A_7D_B2_5E_45
-pacmd set-default-source bluez_card.00_1A_7D_B2_5E_45
+#pacmd set-default-source bluez_sink.00_1A_7D_B2_5E_45.monitor
